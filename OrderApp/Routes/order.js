68fbfc7ae0a8ed4a561730d9f99fe7dc7786f7fe
Faraz Ahmed <farazahmed2197@gmail.com>
2019-03-25 21:25:41 +0500

    update

--- OrderApp/Routes/order.js
+++ OrderApp/Routes/order.js
@@ -1,61 +1,64 @@
 const router = require('express').Router();
 const db = require('../Firebase').db;
 const orderRef = db.collection('Order')
+const request = require('request');
 //route to signup
 router.post('/createOrder', async (req, res) => {
-    
+    let ref = orderRef.doc()
+    let state = "";
     let userId = req.body.userId
     let products = req.body.products
     let amount = req.body.amount
     let cardNumber = req.body.cardNumber
     let csv = req.body.csv
-
-    let ref = orderRef.doc()
     let order = {
-        userId : userId,
-        orderState : 'created',
-        products : products,
-        amount : amount
+        userId: userId,
+        orderState: 'created',
+        products: products,
+        amount: amount
     }
-
-    ref.set(order).then(() => {
+    ref.set(order).then(async () => {
+        let orderId = await ref.get().then(doc => {
+            return doc.id
+        })
         res.status(200).json({
-            success : true,
-            message : "Order Created"
+            success: true,
+            message: "Order Created",
+            orderId: orderId
         })
     }).catch(err => {
         res.status(500).json({
-            success : false,
-            message : "Order not created",
-            error : err
+            success: false,
+            message: "Order not created",
+            error: err
         })
-    })
-
-    makePayment(cardNumber, csv, amount)
-    
+    });
+    const response = await makePayment(cardNumber, csv, amount);
+    response === true ? state = "Confirmed" : state = "Declined";
+    ref.update({
+        orderState: state
+    });
+    response ? await changeState(ref) : null;
 })
 
 router.get('/cancelOrder/:orderId', async (req, res) => {
     let orderId = req.params.orderId
     let ref = orderRef.doc(orderId)
-
     order = {
-        orderState : 'cancelled'
+        orderState: 'cancelled'
     }
-
     ref.update(order).then(() => {
         res.status(200).json({
-            success : true,
-            message : "Order Cancelled"
+            success: true,
+            message: "Order Cancelled"
         })
     }).catch(err => {
         res.status(500).json({
-            success : false,
-            message : "Something goes wrong",
-            error : err
+            success: false,
+            message: "Something goes wrong",
+            error: err
         })
-    })
-    
+    });
 })
 
 router.get('/checkStatus/:orderId', async (req, res) => {
@@ -63,41 +66,57 @@ router.get('/checkStatus/:orderId', async (req, res) => {
     let ref = orderRef.doc(orderId)
 
     ref.get().then((doc) => {
-        if(doc.exists){
+        if (doc.exists) {
             res.status(200).json({
-                success : true,
-                orderStatus : doc.data().orderState
+                success: true,
+                orderStatus: doc.data().orderState
             })
         }
     }).catch(err => {
         res.status(500).json({
-            success : false,
-            message : "Something goes wrong",
-            error : err
+            success: false,
+            message: "Something goes wrong",
+            error: err
         })
     })
-    
+
 })
 
-async function makePayment(cardNumber, csv , amount) {
+async function makePayment(cardNumber, csv, amount) {
     const params = {
-      url: "localhost:4000/payment",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        cardNumber, csv, amount
-      })
+        url: "http://localhost:4000/payment",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            cardNumber,
+            csv,
+            amount
+        })
     };
-    return new Promise(function(resolve, reject) {
-      request.post(params, function(err, res, body) {
-        if (err) {
-          resolve(err);
-          console.log("------error------", err);
-        } else {
-          resolve(JSON.parse(body))
-          console.log("------PAYMENT PROCESS COMPLETED--------" + body + res);
-        }
-      });
+    return new Promise(function (resolve, reject) {
+        request.post(params, function (err, res, body) {
+            if (err) {
+                resolve(err);
+                console.log("------error------", err);
+            } else {
+                resolve(JSON.parse(body))
+                console.log("------PAYMENT PROCESS COMPLETED--------" + body + res);
+            }
+        });
     });
-   }
+}
+
+async function changeState(ref) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            ref.update({
+                orderState: "Delivered"
+            });
+            resolve(null);
+            console.log("UPDATED")
+        }, 5000);
+    });
+}
 
 module.exports = router
--- OrderApp/app.js
+++ OrderApp/app.js
@@ -10,45 +10,45 @@ var cors = require('cors')
 
 app.use(cors())
 //require routes
-const userRoute = require('./Routes/user');
 const orderRoute = require('./Routes/order');
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 //  Header Setup ///////////////////
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers",
-      "Origin , X-Requested-With , Content-Type , Accept , Authorization"
-    );
-    if (req.method === 'OPTIONS') {
-      res.header("Access-Control-Allow-Methods", 'PUT , POST , PATCH , DELETE , GET');
-      return res.status(200).json({});
-    }
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers",
+    "Origin , X-Requested-With , Content-Type , Accept , Authorization"
+  );
+  if (req.method === 'OPTIONS') {
+    res.header("Access-Control-Allow-Methods", 'PUT , POST , PATCH , DELETE , GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 //forward to routes
-app.use('/user', userRoute)
 app.use('/order', orderRoute)
 //Error Handlers    ///////////
 app.use((req, res, next) => {
-    const error = new Error('not found');
-    error.status = 404;
-    next(error);
+  const error = new Error('not found');
+  error.status = 404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    Error: {
+      message: error.message
+    }
   });
-  app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-      Error: {
-        message: error.message
-      }
-    });
-  });
-  ///////////////////////////////////
+});
+///////////////////////////////////
 
-app.listen(PORT , (error) => {
-    if(error) return LOGGER.error(error.meesage);
-    LOGGER.appStarted(PORT ,host);
+app.listen(PORT, (error) => {
+  if (error) return LOGGER.error(error.meesage);
+  LOGGER.appStarted(PORT, host);
 })
